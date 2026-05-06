d6f1ea08b890f041b29e3e7dd79730c21064b253
jithinjnaik94-star <jithinjnaik94@gmail.com>
2026-05-06 10:16:49 +0530

    Create kthsmall.java

---
+++ kthsmall.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the number of elements: ");
+        int n = sc.nextInt();
+        int[] numbers = new int[n];
+        System.out.println("Enter " + n + " numbers:");
+        for (int i = 0; i < n; i++) {
+            numbers[i] = sc.nextInt();
+        }        
+        System.out.print("Enter k: ");
+        int k = sc.nextInt();
+        for (int i = 0; i < n - 1; i++) {
+            for (int j = 0; j < n - i - 1; j++) {
+                if (numbers[j] > numbers[j + 1]) {
+                    int temp = numbers[j];
+                    numbers[j] = numbers[j + 1];
+                    numbers[j + 1] = temp;
+                }
+            }
+        }
+        if (k > 0 && k <= n) {
+            System.out.println("The " + k + "-th smallest element is: " + numbers[k - 1]);
+        } else {
+            System.out.println("Invalid value of k.");
+        }
+    }
+}
